bd5c7fa64bc27a86b16e2d3f08c51611b1cd3b73
liuzwei <liuzhaowei.cool@163.com>
2014-11-11 21:59:31 +0800

    初始化代码，实现登陆功能

--- src/com/app/ebaebo/ui/LoginActivity.java
+++ src/com/app/ebaebo/ui/LoginActivity.java
@@ -1,18 +1,103 @@
 package com.app.ebaebo.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.app.ebaebo.R;
+import com.app.ebaebo.util.InternetAPI;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by liuzwei on 2014/11/11.
  */
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener{
+    private EditText username;//用户名
+    private EditText password;//密码
+    private TextView loginBtn;//登陆按钮
+    private TextView forgetPass;//忘记密码
 
-    TextView username;
+    private RequestQueue mRequestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        initView();
+
+        mRequestQueue = Volley.newRequestQueue(this);
+    }
+
+    private void initView(){
+        username = (EditText) findViewById(R.id.login_username);
+        password = (EditText) findViewById(R.id.login_password);
+        loginBtn = (TextView) findViewById(R.id.login_btn);
+        forgetPass = (TextView) findViewById(R.id.login_forgetpass);
+
+        loginBtn.setOnClickListener(this);
+        forgetPass.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login_btn://登陆按钮
+                String name = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if (name.isEmpty()){
+                    Toast.makeText(this, "用户名不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (pass.isEmpty()){
+                    Toast.makeText(this, "密码不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String jsonParams = "{\"user_name\":"+name+"\"password\":"+pass+"}";
+                JSONObject params = new JSONObject();
+                try {
+                    params.put("user_name", name);
+                    params.put("password", pass);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                JsonObjectRequest jr = new JsonObjectRequest(Request.Method.GET,InternetAPI.LOGIN_API,params,new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println("jsonObject：" + response);
+                        //登陆成功后跳转到主页面
+                        Intent main = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(main);
+                    }
+                },new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+                mRequestQueue.add(jr);
+                break;
+            case R.id.login_forgetpass://忘记密码
+
+                break;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mRequestQueue.cancelAll(this);
     }
 }
---
+++ src/com/app/ebaebo/util/InternetAPI.java
@@ -0,0 +1,12 @@
+package com.app.ebaebo.util;
+
+/**
+ * Created by liuzwei on 2014/11/11.
+ */
+public class InternetAPI {
+    //登陆接口  get方式
+   public static String LOGIN_API = "http://yey.xqb668.com/json.php/user.api-login/";
+
+    //注册接口  get方式
+    public static String REGISTER_PAI = "http://yey.xqb668.com/json.php/user.api-regist/ ";
+}
