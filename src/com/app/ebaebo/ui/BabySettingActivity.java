a2d69840c26ae8de32c24c4d7bc15416e1e479da
long <826321978@qq.com>
2014-11-19 20:47:39 +0800

    相册和育英信息

--- src/com/app/ebaebo/ui/BabySettingActivity.java
+++ src/com/app/ebaebo/ui/BabySettingActivity.java
@@ -2,31 +2,51 @@ package com.app.ebaebo.ui;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import com.app.ebaebo.R;
 
 /**
  * Created by liuzwei on 2014/11/14.
  */
 public class BabySettingActivity extends BaseActivity implements View.OnClickListener{
-    ImageView back;
+    ImageView back;//返回按钮
+    private ImageView tx;//头像
+    private EditText nameText;//name
+    private EditText guanxiText;//关系
+    private TextView set;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.baby_setting);
+        setContentView(R.layout.baobao_setting);
         initView();
     }
 
     private void initView(){
-        back = (ImageView) findViewById(R.id.baby_setting_back);
+        back = (ImageView) findViewById(R.id.back);
         back.setOnClickListener(this);
+        set = (TextView) this.findViewById(R.id.set);
+        set.setOnClickListener(this);
+        tx = (ImageView) this.findViewById(R.id.tx);
+        tx.setOnClickListener(this);
+        nameText = (EditText) this.findViewById(R.id.name);
+        guanxiText = (EditText) this.findViewById(R.id.guanxi);
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.baby_setting_back:
+            case R.id.back:
                 finish();
+                break;
+            case R.id.set:
+                //设置
+                break;
+            case R.id.tx:
+                //头像设置
                 break;
         }
     }
--- src/com/app/ebaebo/ui/MumSettingActivity.java
+++ src/com/app/ebaebo/ui/MumSettingActivity.java
@@ -13,24 +13,28 @@ import com.app.ebaebo.R;
 public class MumSettingActivity extends BaseActivity implements View.OnClickListener{
     private ImageView back;//返回
     private ImageView tx;//头像
-    private EditText name;
-    private EditText guanxi;
-    private TextView set;
+    private EditText zhanghao;//账号
+    private EditText mobile;//手机
+    private EditText password;//密码
+    private TextView set;//设置
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mum_setting);
+        setContentView(R.layout.mm_setting);
         initView();
     }
 
     private void initView(){
         back = (ImageView) findViewById(R.id.back);
         back.setOnClickListener(this);
-        name = (EditText) this.findViewById(R.id.name);
-        guanxi = (EditText) this.findViewById(R.id.guanxi);
+        zhanghao = (EditText) this.findViewById(R.id.zhanghao);
+        mobile = (EditText) this.findViewById(R.id.mobile);
+        password = (EditText) this.findViewById(R.id.password);
         set = (TextView) this.findViewById(R.id.set);
         set.setOnClickListener(this);
+        tx = (ImageView) this.findViewById(R.id.tx);
+        tx.setOnClickListener(this);
     }
 
     @Override
@@ -41,6 +45,9 @@ public class MumSettingActivity extends BaseActivity implements View.OnClickList
                 break;
             case R.id.set:
                 //设置妈妈
+                break;
+            case R.id.tx:
+                //设置头像
                 break;
         }
     }
