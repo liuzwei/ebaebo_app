0e04717cca297dc2438305493fce3005a528e26e
liuzwei <liuzhaowei.cool@163.com>
2014-11-22 22:22:34 +0800

    添加成长记录发布页

--- src/com/app/ebaebo/ui/MainActivity.java
+++ src/com/app/ebaebo/ui/MainActivity.java
@@ -1,8 +1,11 @@
 package com.app.ebaebo.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.*;
 import com.android.volley.Request;
@@ -45,6 +48,12 @@ public class MainActivity extends BaseActivity implements
     private TextView setting;//设置
     private Spinner growingManager;//成长管理下拉
     private ArrayAdapter<String> spinnerAdapter;
+    private RelativeLayout footLayout;
+    private RelativeLayout text;//文字
+    private RelativeLayout photo;//拍照
+    private RelativeLayout video;//摄像
+    private RelativeLayout record;//录音
+    private RelativeLayout picture;// 图库
 
     private ContentListView listView;
     private GrowingAdapter adapter;
@@ -52,7 +61,7 @@ public class MainActivity extends BaseActivity implements
     private String uid;
     private int pageIndex;
     private int pageSize;
-    private int child_id;
+    private String child_id;
     private Account account;
 
     private List<Growing> growingList = new ArrayList<Growing>();
@@ -119,8 +128,20 @@ public class MainActivity extends BaseActivity implements
         callName = (TextView) slideMenu.findViewById(R.id.leftmenu_callname);
         setting = (TextView) slideMenu.findViewById(R.id.leftmenu_setting);
         growingManager = (Spinner) slideMenu.findViewById(R.id.growing_manager_spinner);
-
         listView = (ContentListView) slideMenu.findViewById(R.id.index_pull_refresh_lsv);
+        footLayout = (RelativeLayout) slideMenu.findViewById(R.id.index_foot_layout);
+        text = (RelativeLayout) footLayout.findViewById(R.id.foot_content);
+        photo = (RelativeLayout) footLayout.findViewById(R.id.foot_photo);
+        video = (RelativeLayout) footLayout.findViewById(R.id.foot_video);
+        record = (RelativeLayout) footLayout.findViewById(R.id.foot_record);
+        picture = (RelativeLayout) footLayout.findViewById(R.id.foot_picture);
+
+        text.setOnClickListener(this);
+        photo.setOnClickListener(this);
+        video.setOnClickListener(this);
+        record.setOnClickListener(this);
+        picture.setOnClickListener(this);
+
         listView.setOnRefreshListener(this);
         listView.setOnLoadListener(this);
 
@@ -185,6 +206,22 @@ public class MainActivity extends BaseActivity implements
                 Intent setting = new Intent(MainActivity.this, SettingActivity.class);
                 startActivity(setting);
                 break;
+            case R.id.foot_content://文字
+                Intent textIntent = new Intent(MainActivity.this, PublishImageActivity.class);
+                startActivity(textIntent);
+                break;
+            case R.id.foot_photo://拍照
+
+                break;
+            case R.id.foot_video://摄像
+
+                break;
+            case R.id.foot_record://录音
+
+                break;
+            case R.id.foot_picture://图库
+
+                break;
         }
     }
 
@@ -206,7 +243,7 @@ public class MainActivity extends BaseActivity implements
     }
 
     private void getData(final int tag){
-        String uri = String.format(InternetURL.GROWING_MANAGER_API+"?uid=%s&pageIndex=%d&pageSize=%d&child_id=%d",uid, pageIndex, pageSize, child_id);
+        String uri = String.format(InternetURL.GROWING_MANAGER_API+"?uid=%s&pageIndex=%d&pageSize=%d&child_id=%s",uid, pageIndex, pageSize, child_id);
         StringRequest request = new StringRequest(Request.Method.GET,
                 uri,
                 new Response.Listener<String>() {
@@ -274,10 +311,10 @@ public class MainActivity extends BaseActivity implements
                                 @Override
                                 public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                                     if (position == 0) {
-                                        child_id = 0;
+                                        child_id = "";
                                     } else{
                                         Baby baby = babies.get(position-1);
-                                        child_id = Integer.parseInt(baby.getId());
+                                        child_id = baby.getId();
                                     }
                                     getData(ContentListView.REFRESH);
 
@@ -319,5 +356,40 @@ public class MainActivity extends BaseActivity implements
     public void onLoad() {
         pageIndex++;
         getData(ContentListView.LOAD);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            showTips();
+            return false;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void showTips() {
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this).setTitle("提醒")
+                .setMessage("是否退出程序")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                    }
+
+                }).setNegativeButton("取消",
+
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                return;
+                            }
+                        }).create(); // 创建对话框
+        alertDialog.show(); // 显示对话框
     }
 }
---
+++ src/com/app/ebaebo/ui/PublishImageActivity.java
@@ -0,0 +1,130 @@
+package com.app.ebaebo.ui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.app.ebaebo.R;
+import com.app.ebaebo.data.BabyDATA;
+import com.app.ebaebo.entity.Account;
+import com.app.ebaebo.entity.Baby;
+import com.app.ebaebo.util.InternetURL;
+import com.app.ebaebo.widget.ContentListView;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by liuzwei on 2014/11/22.
+ */
+public class PublishImageActivity extends BaseActivity implements View.OnClickListener{
+    private ImageView back;
+    private TextView publish;
+    private Spinner spinner;
+    private EditText content;
+    private GridView gridView;
+    private ArrayAdapter<String> spinnerAdapter;
+
+    private Account account;
+    private int res[] = new int[]{R.drawable.abaose};
+    private List<Baby> babies = new ArrayList<Baby>();//下拉列表宝宝
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.publish_image_layout);
+        initView();
+        account = getGson().fromJson(sp.getString(Constants.ACCOUNT_KEY, ""), Account.class);
+        getBabyList();
+
+        List<Map<String, Object>> data = new ArrayList<Map<String,Object>>();
+        for(int i=0;i<res.length;i++){
+            Map<String,Object> map = new HashMap<String, Object>();
+            map.put("imageView",res[i]);
+            data.add(map);
+        }
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, data, R.layout.image_cell_layout , new String[]{"imageView"}, new int[]{R.id.imageView1});
+        gridView.setAdapter(simpleAdapter);
+    }
+
+    private void initView(){
+        back = (ImageView) findViewById(R.id.publish_image_back);
+        publish = (TextView) findViewById(R.id.publish_image_run);
+        spinner = (Spinner) findViewById(R.id.publish_image_spinner);
+        content = (EditText) findViewById(R.id.publish_image_content);
+        gridView = (GridView) findViewById(R.id.publish_image_gv);
+
+        back.setOnClickListener(this);
+        publish.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.publish_image_back://后退按钮
+                finish();
+                break;
+            case R.id.publish_image_run://发布按钮
+
+                break;
+        }
+    }
+
+    private void  getBabyList(){
+
+        String uri = String.format(InternetURL.GET_BABY_URL +"?uid=%s", account.getUid());
+//        String uri = "http://yey.xqb668.com/json.php/growing.api-childrens/?uid=102";
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                uri,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        Gson gson = new Gson();
+                        try{
+                            BabyDATA data = gson.fromJson(s, BabyDATA.class);
+                            babies.addAll(data.getData());
+                            List<String> names = new ArrayList<String>();
+                            for (int i=0; i<babies.size(); i++){
+                                names.add(babies.get(i).getName());
+                            }
+                            spinnerAdapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_spinner_item, names);
+                            spinnerAdapter.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
+                            spinner.setAdapter(spinnerAdapter);
+                            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                @Override
+                                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                    Baby baby = babies.get(position);
+                                }
+
+                                @Override
+                                public void onNothingSelected(AdapterView<?> parent) {
+
+                                }
+                            });
+                        }catch (Exception e){
+//                            ErrorDATA data = gson.fromJson(s, ErrorDATA.class);
+//                            if (data.getCode() == 500){
+//                                Log.i("ErrorData", "获取baby信息数据错误");
+//                            }
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+                    }
+                }
+        );
+        getRequestQueue().add(request);
+    }
+}
