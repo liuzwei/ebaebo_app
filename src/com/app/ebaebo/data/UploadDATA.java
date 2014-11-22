8a20df34bb3c05dc3ef806fdd99b505de87f7655
liuzwei <liuzhaowei.cool@163.com>
2014-11-22 16:40:25 +0800

    添加设置头像功能

---
+++ src/com/app/ebaebo/data/UploadDATA.java
@@ -0,0 +1,34 @@
+package com.app.ebaebo.data;
+
+/**
+ * Created by liuzwei on 2014/11/22.
+ */
+public class UploadDATA {
+    private int code;
+    private String msg;
+    private String url;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
--- src/com/app/ebaebo/ui/MumSettingActivity.java
+++ src/com/app/ebaebo/ui/MumSettingActivity.java
@@ -1,40 +1,114 @@
 package com.app.ebaebo.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.app.ebaebo.EbaeboApplication;
 import com.app.ebaebo.R;
+import com.app.ebaebo.adapter.AnimateFirstDisplayListener;
+import com.app.ebaebo.data.ErrorDATA;
+import com.app.ebaebo.data.UploadDATA;
+import com.app.ebaebo.entity.Account;
+import com.app.ebaebo.util.CommonUtil;
+import com.app.ebaebo.util.CompressPhotoUtil;
+import com.app.ebaebo.util.InternetURL;
+import com.app.ebaebo.util.StringUtil;
+import com.app.ebaebo.util.upload.MultiPartStack;
+import com.app.ebaebo.util.upload.MultiPartStringRequest;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by liuzwei on 2014/11/14.
  */
 public class MumSettingActivity extends BaseActivity implements View.OnClickListener{
+    private static final String TAG = MumSettingActivity.class.getSimpleName();
     private ImageView back;//返回
     private ImageView tx;//头像
-    private EditText zhanghao;//账号
-    private EditText mobile;//手机
-    private EditText password;//密码
+    private EditText name;
+    private String nickName;
+//    private EditText zhanghao;//账号
+//    private EditText mobile;//手机
+//    private EditText password;//密码
     private TextView set;//设置
+    TextView settingTitle;
+
+    private Account account;
+    private static RequestQueue mSingleQueue;
+    private String pics = "";
+    private String identity;
+
+    ImageLoader imageLoader = ImageLoader.getInstance();//图片加载类
+    private ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
+
+    private static final File PHOTO_CACHE_DIR = new File(Environment.getExternalStorageDirectory() + "/ebaebo/PhotoCache");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mm_setting);
+        mSingleQueue = Volley.newRequestQueue(this, new MultiPartStack());
+        account = getGson().fromJson(sp.getString(Constants.ACCOUNT_KEY, ""), Account.class);
+        identity = getGson().fromJson(sp.getString(Constants.IDENTITY, ""), String.class);
         initView();
+        initData();
     }
+
+
 
     private void initView(){
         back = (ImageView) findViewById(R.id.back);
         back.setOnClickListener(this);
-        zhanghao = (EditText) this.findViewById(R.id.zhanghao);
-        mobile = (EditText) this.findViewById(R.id.mobile);
-        password = (EditText) this.findViewById(R.id.password);
-        set = (TextView) this.findViewById(R.id.set);
+//        zhanghao = (EditText) this.findViewById(R.id.zhanghao);
+//        mobile = (EditText) this.findViewById(R.id.mobile);
+//        password = (EditText) this.findViewById(R.id.password);
+        set = (TextView) this.findViewById(R.id.mum_setting_sure);
         set.setOnClickListener(this);
-        tx = (ImageView) this.findViewById(R.id.tx);
+        tx = (ImageView) this.findViewById(R.id.mum_setting_tx);
         tx.setOnClickListener(this);
+        name = (EditText) this.findViewById(R.id.mum_setting_name);
+        settingTitle = (TextView) findViewById(R.id.mum_setting_title);
+    }
+
+    private void initData(){
+        if (!StringUtil.isNullOrEmpty(identity)){
+            switch (Integer.parseInt(identity)){
+                case 0:
+                    settingTitle.setText("爸爸设置");
+                    imageLoader.displayImage(account.getF_cover(), tx, EbaeboApplication.txOptions, animateFirstListener);
+                    name.setText(account.getF_name());
+                    break;
+                case 1:
+                    settingTitle.setText("妈妈设置");
+                    imageLoader.displayImage(account.getM_cover(), tx, EbaeboApplication.txOptions, animateFirstListener);
+                    name.setText(account.getM_name());
+                    break;
+            }
+        }
     }
 
     @Override
@@ -43,12 +117,223 @@ public class MumSettingActivity extends BaseActivity implements View.OnClickList
             case R.id.back:
                 finish();
                 break;
-            case R.id.set:
-                //设置妈妈
+            case R.id.mum_setting_sure:
+                //设置妈妈  上传头像并保存信息
+                nickName = name.getText().toString();
+                if (StringUtil.isNullOrEmpty(nickName)){
+                    Toast.makeText(mContext, "姓名不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (!StringUtil.isNullOrEmpty(pics)) {
+                    Map<String, File> files = new HashMap<String, File>();
+                    files.put("file", new File(pics));
+                    Map<String, String> params = new HashMap<String, String>();
+                    addPutUploadFileRequest(
+                            InternetURL.UPLOAD_FILE,
+                            files,
+                            params,
+                            mResonseListenerString,
+                            mErrorListener,
+                            null);
+                }else {
+                    setting(null);
+                }
                 break;
-            case R.id.tx:
+            case R.id.mum_setting_tx:
                 //设置头像
+                ShowPickDialog();
                 break;
         }
+    }
+
+    Response.ErrorListener mErrorListener = new Response.ErrorListener() {
+
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            if (error != null) {
+                if (error.networkResponse != null)
+                    Log.i(TAG, " error " + new String(error.networkResponse.data));
+            }
+        }
+    };
+
+    Response.Listener<String> mResonseListenerString = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            if (CommonUtil.isJson(response)){
+                UploadDATA data = getGson().fromJson(response, UploadDATA.class);
+                setting(data.getUrl());
+            }
+            Log.i(TAG, " on response String" + response.toString());
+        }
+    };
+
+    private void setting(String cover){
+
+        String type = getGson().fromJson(sp.getString(Constants.IDENTITY, ""), String.class);
+        String uri = String.format(InternetURL.FATHER_MOTHER_SETTING + "?uid=%s&type=%s&name=%s&cover=%s",account.getUid(), type, nickName, cover);
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                uri,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        if (CommonUtil.isJson(s)) {
+                            ErrorDATA errorDATA = getGson().fromJson(s, ErrorDATA.class);
+                            if (errorDATA.getCode() == 200){
+                                resetAccount(name.getText().toString(), pics);
+                                Toast.makeText(mContext, "设置成功", Toast.LENGTH_SHORT).show();
+                            }else {
+                                Toast.makeText(mContext, "设置失败，请稍后重试", Toast.LENGTH_SHORT).show();
+                            }
+                        }else {
+                            Toast.makeText(mContext, "设置失败，请稍后重试", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Toast.makeText(mContext, "设置失败，请稍后重试", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+        mSingleQueue.add(request);
+    }
+
+    private void resetAccount(String name, String cover){
+        if (!StringUtil.isNullOrEmpty(identity)) {
+            switch (Integer.parseInt(identity)) {
+                case 0://爸爸
+                    account.setF_cover(cover);
+                    account.setF_name(name);
+                    break;
+                case 1://妈妈
+                    account.setM_cover(cover);
+                    account.setM_name(name);
+                    break;
+            }
+            save(Constants.ACCOUNT_KEY, account);
+        }
+    }
+
+    /**
+     * 选择提示对话框
+     */
+    private void ShowPickDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("设置头像...")
+                .setNegativeButton("相册", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        Intent intent = new Intent(Intent.ACTION_PICK, null);
+                        intent.setDataAndType(
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                "image/*");
+                        startActivityForResult(intent, 1);
+
+                    }
+                })
+                .setPositiveButton("拍照", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.dismiss();
+                        Intent intent = new Intent(
+                                MediaStore.ACTION_IMAGE_CAPTURE);
+                        //下面这句指定调用相机拍照后的照片存储的路径
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri
+                                .fromFile(new File(Environment
+                                        .getExternalStorageDirectory(),
+                                        "ebaeboTx.jpg")));
+                        startActivityForResult(intent, 2);
+                    }
+                }).show()
+                .setCanceledOnTouchOutside(true);//点击弹框外部，弹框消失
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            // 如果是直接从相册获取
+            case 1:
+                if(data!=null){
+                    startPhotoZoom(data.getData());
+                }
+                break;
+            // 如果是调用相机拍照时
+            case 2:
+                File temp = new File(Environment.getExternalStorageDirectory()
+                        + "/ebaeboTx.jpg");
+                startPhotoZoom(Uri.fromFile(temp));
+                break;
+            // 取得裁剪后的图片
+            case 3:
+                if(data != null){
+                    setPicToView(data);
+                }
+                break;
+            default:
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * 裁剪图片方法实现
+     * @param uri
+     */
+    public void startPhotoZoom(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, 3);
+    }
+
+    /**
+     * 保存裁剪之后的图片数据
+     * @param picdata
+     */
+    private void setPicToView(Intent picdata) {
+        Bundle extras = picdata.getExtras();
+        if (extras != null) {
+            Bitmap photo = extras.getParcelable("data");
+            Drawable drawable = new BitmapDrawable(photo);
+            if(photo!=null){
+                pics = CompressPhotoUtil.saveBitmap2file(photo, System.currentTimeMillis() + ".jpg", PHOTO_CACHE_DIR);
+                tx.setImageBitmap(photo);
+            }
+        }
+    }
+
+    public static void addPutUploadFileRequest(final String url,
+                                               final Map<String, File> files, final Map<String, String> params,
+                                               final Response.Listener<String> responseListener, final Response.ErrorListener errorListener,
+                                               final Object tag) {
+        if (null == url || null == responseListener) {
+            return;
+        }
+
+        MultiPartStringRequest multiPartRequest = new MultiPartStringRequest(
+                Request.Method.POST, url, responseListener, errorListener) {
+
+            @Override
+            public Map<String, File> getFileUploads() {
+                return files;
+            }
+
+            @Override
+            public Map<String, String> getStringUploads() {
+                return params;
+            }
+
+        };
+
+        Log.i(TAG, " volley put : uploadFile " + url);
+
+        mSingleQueue.add(multiPartRequest);
     }
 }
