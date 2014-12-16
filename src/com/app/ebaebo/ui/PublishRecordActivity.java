dd0771d76684c1d898c6e29606cb1ac16b3151dd
liuzwei <liuzhaowei.cool@163.com>
2014-12-16 10:03:09 +0800

    修改录音页面

--- src/com/app/ebaebo/ui/PublishRecordActivity.java
+++ src/com/app/ebaebo/ui/PublishRecordActivity.java
@@ -4,9 +4,13 @@ import android.app.ProgressDialog;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.*;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -35,8 +39,8 @@ import java.util.*;
 public class PublishRecordActivity extends BaseActivity implements View.OnClickListener {
     private ImageView back;
     private TextView publish;
-    private ImageView play;
-    private ImageView cancel;
+//    private ImageView play;
+//    private ImageView cancel;
     private Button voice;
     private EditText content;
 
@@ -56,41 +60,147 @@ public class PublishRecordActivity extends BaseActivity implements View.OnClickL
     private static final String PATH = "/Recorder.mp3";
     private MP3Recorder recorder = new MP3Recorder(8000);
 
+    private RelativeLayout recordRelativeLayout;
+    private ImageView mRecordLight_1;
+    private ImageView mRecordLight_2;
+    private ImageView mRecordLight_3;
+
+    private Animation mRecordLight_1_Animation;
+    private Animation mRecordLight_2_Animation;
+    private Animation mRecordLight_3_Animation;
+
+    private static final int RECORD_NO = 0; // 不在录音
+    private static final int RECORD_ING = 1; // 正在录音
+    private static final int RECORD_ED = 2; // 完成录音
+    private int mRecord_State = 0; // 录音的状态
+    private boolean mPlayState; // 播放状态
+    private int mPlayCurrentPosition;// 当前播放的时间
+
+
+    private TextView deleteVoice;
+    private LinearLayout voicePlayLayout;
+    private ImageView voicePlay;
+    private ProgressBar voiceProgressbar;
+    private TextView voiceTime;
+
+    /**
+     * 用来控制动画效果
+     */
+    Handler mRecordLightHandler = new Handler() {
+
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 0:
+                    if (mRecord_State == RECORD_ING) {
+                        mRecordLight_1.setVisibility(View.VISIBLE);
+                        mRecordLight_1_Animation = AnimationUtils.loadAnimation(
+                                PublishRecordActivity.this, R.anim.voice_anim);
+                        mRecordLight_1.setAnimation(mRecordLight_1_Animation);
+                        mRecordLight_1_Animation.startNow();
+                    }
+                    break;
+
+                case 1:
+                    if (mRecord_State == RECORD_ING) {
+                        mRecordLight_2.setVisibility(View.VISIBLE);
+                        mRecordLight_2_Animation = AnimationUtils.loadAnimation(
+                                PublishRecordActivity.this, R.anim.voice_anim);
+                        mRecordLight_2.setAnimation(mRecordLight_2_Animation);
+                        mRecordLight_2_Animation.startNow();
+                    }
+                    break;
+                case 2:
+                    if (mRecord_State == RECORD_ING) {
+                        mRecordLight_3.setVisibility(View.VISIBLE);
+                        mRecordLight_3_Animation = AnimationUtils.loadAnimation(
+                                PublishRecordActivity.this, R.anim.voice_anim);
+                        mRecordLight_3.setAnimation(mRecordLight_3_Animation);
+                        mRecordLight_3_Animation.startNow();
+                    }
+                    break;
+                case 3:
+                    if (mRecordLight_1_Animation != null) {
+                        mRecordLight_1.clearAnimation();
+                        mRecordLight_1_Animation.cancel();
+                        mRecordLight_1.setVisibility(View.GONE);
+
+                    }
+                    if (mRecordLight_2_Animation != null) {
+                        mRecordLight_2.clearAnimation();
+                        mRecordLight_2_Animation.cancel();
+                        mRecordLight_2.setVisibility(View.GONE);
+                    }
+                    if (mRecordLight_3_Animation != null) {
+                        mRecordLight_3.clearAnimation();
+                        mRecordLight_3_Animation.cancel();
+                        mRecordLight_3.setVisibility(View.GONE);
+                    }
+
+                    break;
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.publish_record_layout);
+        setContentView(R.layout.record_activity);
         account = getGson().fromJson(sp.getString(Constants.ACCOUNT_KEY, ""), Account.class);
         getBabyList();
         initView();
     }
 
     private void initView(){
+        recordRelativeLayout = (RelativeLayout) this.findViewById(R.id.record_relative_layout);
         back = (ImageView) findViewById(R.id.publish_record_back);
+        deleteVoice = (TextView) this.findViewById(R.id.record_activity_delete);
+        deleteVoice.setOnClickListener(this);
         publish = (TextView) findViewById(R.id.publish_record_run);
-        play = (ImageView) findViewById(R.id.publish_record_play);
-        voice = (Button) findViewById(R.id.publish_record_voice);
+//        play = (ImageView) findViewById(R.id.publish_record_play);
+        voice = (Button) findViewById(R.id.voice_record_btn);
         spinner = (Spinner) findViewById(R.id.publish_record_spinner);
         content = (EditText) findViewById(R.id.publish_record_content);
-        cancel = (ImageView) findViewById(R.id.publish_record_cancel);
+//        cancel = (ImageView) findViewById(R.id.publish_record_cancel);
 
-        play.setVisibility(View.GONE);
-        cancel.setVisibility(View.GONE);
-        cancel.setOnClickListener(this);
+        mRecordLight_1 = (ImageView) findViewById(R.id.voice_recordinglight_1);
+        mRecordLight_2 = (ImageView) findViewById(R.id.voice_recordinglight_2);
+        mRecordLight_3 = (ImageView) findViewById(R.id.voice_recordinglight_3);
+
+        voicePlayLayout = (LinearLayout) this.findViewById(R.id.voice_display_voice_layout);
+        voicePlay = (ImageView) this.findViewById(R.id.voice_display_voice_play);
+        voiceProgressbar = (ProgressBar) this.findViewById(R.id.voice_display_voice_progressbar);
+        voiceTime = (TextView) this.findViewById(R.id.voice_display_voice_time);
+         voicePlay.setOnClickListener(this);
+
+//        play.setVisibility(View.GONE);
+//        cancel.setVisibility(View.GONE);
+//        cancel.setOnClickListener(this);
         back.setOnClickListener(this);
         publish.setOnClickListener(this);
-        play.setOnClickListener(this);
+//        play.setOnClickListener(this);
         voice.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()){
                     case MotionEvent.ACTION_DOWN:
-                        recorder.start(Environment.getExternalStorageDirectory()  + PATH);
+                        // 开始动画效果
+                        startRecordLightAnimation();
+                        mRecord_State = RECORD_ING;
+                        recorder.start(Environment.getExternalStorageDirectory() + PATH);
                         break;
                     case MotionEvent.ACTION_UP:
-                        play.setVisibility(View.VISIBLE);
-                        cancel.setVisibility(View.VISIBLE);
-                        recorder.stop();
+                            // 停止动画效果
+                            stopRecordLightAnimation();
+                            // 修改录音状态
+                            recordRelativeLayout.setVisibility(View.GONE);
+                            mRecord_State = RECORD_ED;
+                            recorder.stop();
+
+                        voicePlayLayout.setVisibility(View.VISIBLE);
+                        voicePlay.setImageResource(R.drawable.globle_player_btn_play);
+                        voiceProgressbar.setMax(CommonUtil.getMp3TrackLength(new File(Environment.getExternalStorageDirectory() + PATH)));
+                        voiceProgressbar.setProgress(0);
+                        voiceTime.setText(CommonUtil.getMp3TrackLength(new File(Environment.getExternalStorageDirectory() + PATH)) + "″");
                         break;
                     default:
                         break;
@@ -149,44 +259,21 @@ public class PublishRecordActivity extends BaseActivity implements View.OnClickL
         );
         getRequestQueue().add(request);
     }
+    /**
+     * 开始动画效果
+     */
+    private void startRecordLightAnimation() {
+        mRecordLightHandler.sendEmptyMessageDelayed(0, 0);
+        mRecordLightHandler.sendEmptyMessageDelayed(1, 1000);
+        mRecordLightHandler.sendEmptyMessageDelayed(2, 2000);
+    }
 
-//    private void startVoice(){
-//// 设置录音保存路径
-//        mFileName = PATH + UUID.randomUUID().toString() + ".amr";
-//        String state = android.os.Environment.getExternalStorageState();
-//        if (!state.equals(android.os.Environment.MEDIA_MOUNTED)) {
-//            Log.i(LOG_TAG, "SD Card is not mounted,It is  " + state + ".");
-//        }
-//        File directory = new File(mFileName).getParentFile();
-//        if (!directory.exists() && !directory.mkdirs()) {
-//            Log.i(LOG_TAG, "Path to file could not be created");
-//        }
-//        mRecorder = new MediaRecorder();
-//        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-//        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
-//        mRecorder.setOutputFile(mFileName);
-//        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
-//        try {
-//            mRecorder.prepare();
-//        } catch (IOException e) {
-//            Log.e(LOG_TAG, "prepare() failed");
-//        }
-//        mRecorder.start();
-//    }
-
-//    private void stopVoice(){
-//        try {
-//            mRecorder.stop();
-//            play.setVisibility(View.VISIBLE);
-//            cancel.setVisibility(View.VISIBLE);
-//        }catch (Exception e){
-//            Log.i("PublishRecordActivity", "录音时间太短，为0， 无法保存");
-//        }finally {
-//            mRecorder.release();
-//            mRecorder = null;
-//        }
-////        Toast.makeText(getApplicationContext(), "保存录音" + mFileName, 0).show();
-//    }
+    /**
+     * 停止动画效果
+     */
+    private void stopRecordLightAnimation() {
+        mRecordLightHandler.sendEmptyMessage(3);
+    }
 
     @Override
     public void onClick(View v) {
@@ -194,29 +281,87 @@ public class PublishRecordActivity extends BaseActivity implements View.OnClickL
             case R.id.publish_record_back://返回
                 finish();
                 break;
-            case R.id.publish_record_play://播放
-                try {
-                    mPlayer.reset();
-                    mPlayer.setDataSource(Environment.getExternalStorageDirectory() + PATH);
-                    mPlayer.prepare();
-                    mPlayer.start();
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, "播放失败");
-                }
-                break;
             case R.id.publish_record_run://发布
                 progressDialog = new ProgressDialog(PublishRecordActivity.this);
                 progressDialog.setMessage("正在发布，请稍后");
                 progressDialog.show();
                 uploadRecord();
                 break;
-            case R.id.publish_record_cancel:
+            case R.id.record_activity_delete:
                 File file = new File(Environment.getExternalStorageDirectory() + PATH);
                 if (file.isFile() && file.exists()){
                     file.delete();
-                    play.setVisibility(View.GONE);
-                    cancel.setVisibility(View.GONE);
+                    voicePlayLayout.setVisibility(View.GONE);
+//                    play.setVisibility(View.GONE);
+//                    cancel.setVisibility(View.GONE);
                     Toast.makeText(mContext, "删除文件成功", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.voice_display_voice_play://播放音乐
+// 播放录音
+                if (!mPlayState) {
+                    mPlayer = new MediaPlayer();
+                    try {
+                        // 添加录音的路径
+                        mPlayer.setDataSource(Environment.getExternalStorageDirectory() + PATH);
+                        // 准备
+                        mPlayer.prepare();
+                        // 播放
+                        mPlayer.start();
+                        // 根据时间修改界面
+                        new Thread(new Runnable() {
+                            public void run() {
+                                voiceProgressbar.setMax(CommonUtil.getMp3TrackLength(new File(Environment.getExternalStorageDirectory() + PATH)));
+                                mPlayCurrentPosition = 0;
+                                while (mPlayer.isPlaying()) {
+                                    mPlayCurrentPosition = mPlayer.getCurrentPosition() / 1000;
+                                    voiceProgressbar.setProgress(mPlayCurrentPosition);
+                                }
+                            }
+                        }).start();
+                        // 修改播放状态
+                        mPlayState = true;
+                        // 修改播放图标
+                        voicePlay.setImageResource(R.drawable.globle_player_btn_stop);
+
+                        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                            // 播放结束后调用
+                            public void onCompletion(MediaPlayer mp) {
+                                // 停止播放
+                                mPlayer.stop();
+                                // 修改播放状态
+                                mPlayState = false;
+                                // 修改播放图标
+                                voicePlay.setImageResource(R.drawable.globle_player_btn_play);
+                                // 初始化播放数据
+                                mPlayCurrentPosition = 0;
+                                voiceProgressbar.setProgress(mPlayCurrentPosition);
+                            }
+                        });
+
+                    } catch (IllegalArgumentException e) {
+                        e.printStackTrace();
+                    } catch (IllegalStateException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    if (mPlayer != null) {
+                        // 根据播放状态修改显示内容
+                        if (mPlayer.isPlaying()) {
+                            mPlayState = false;
+                            mPlayer.stop();
+                            voicePlay.setImageResource(R.drawable.globle_player_btn_play);
+                            mPlayCurrentPosition = 0;
+                            voiceProgressbar.setProgress(mPlayCurrentPosition);
+                        } else {
+                            mPlayState = false;
+                            voicePlay.setImageResource(R.drawable.globle_player_btn_play);
+                            mPlayCurrentPosition = 0;
+                            voiceProgressbar.setProgress(mPlayCurrentPosition);
+                        }
+                    }
                 }
                 break;
         }
